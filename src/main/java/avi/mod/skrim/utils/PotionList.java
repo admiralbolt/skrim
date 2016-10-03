ff4f21ced0458d18f1e295ed08cd282920dd6ffb
aknecht2 <aknecht2@unl.edu>
2016-10-03 18:00:45 -0500

    Made a PotionList class to make my life easier.

---
+++ src/main/java/avi/mod/skrim/utils/PotionList.java
@@ -0,0 +1,40 @@
+package avi.mod.skrim.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.potion.Potion;
+
+public class PotionList {
+	
+	public static Potion SPEED = Potion.getPotionById(1);
+	public static Potion SLOWNESS = Potion.getPotionById(2);
+	public static Potion HASTE = Potion.getPotionById(3);
+	public static Potion FATIGUE = Potion.getPotionById(4);
+	public static Potion STRENGTH = Potion.getPotionById(5);
+	public static Potion INSTANTHEALTH = Potion.getPotionById(6);
+	public static Potion INSTANTDAMAGE = Potion.getPotionById(7);
+	public static Potion JUMPBOOST = Potion.getPotionById(8);
+	public static Potion NAUSEAU = Potion.getPotionById(19);
+	public static Potion REGENERATION = Potion.getPotionById(10);
+	public static Potion RESISTANCE = Potion.getPotionById(11);
+	public static Potion FIRERESISTANCE = Potion.getPotionById(12);
+	public static Potion WATERBREATHING = Potion.getPotionById(13);
+	public static Potion INVISIBILITY = Potion.getPotionById(14);
+	public static Potion BLINDNESS = Potion.getPotionById(15);
+	public static Potion NIGHTVISION = Potion.getPotionById(16);
+	public static Potion HUNGER = Potion.getPotionById(17);
+	public static Potion WEAKNESS = Potion.getPotionById(18);
+	public static Potion POISON = Potion.getPotionById(19);
+	public static Potion WITHER = Potion.getPotionById(20);
+	public static Potion HEALTHBOOST = Potion.getPotionById(21);
+	public static Potion ABSORPTION = Potion.getPotionById(22);
+	public static Potion SATURATION = Potion.getPotionById(23);
+	public static Potion GLOWING = Potion.getPotionById(24);
+	public static Potion LEVITATION = Potion.getPotionById(25);
+	public static Potion LUCK = Potion.getPotionById(26);
+	public static Potion BADLUCK = Potion.getPotionById(27);
+
+	
+
+}
