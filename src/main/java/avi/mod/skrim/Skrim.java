3caabd01da80ee5013f8c310c708c47360c27d46
aknecht2 <aknecht2@unl.edu>
2017-02-06 19:49:47 -0600

    Updated minecraft / skrim versions.

--- src/main/java/avi/mod/skrim/Skrim.java
+++ src/main/java/avi/mod/skrim/Skrim.java
@@ -1,21 +1,16 @@
 package avi.mod.skrim;
 
-import avi.mod.skrim.items.ModItems;
-import avi.mod.skrim.proxy.CommonProxy;
 import avi.mod.skrim.client.TestTab;
 import avi.mod.skrim.commands.CommandRegistry;
-import avi.mod.skrim.commands.SetSkillCommand;
-import net.minecraft.stats.Achievement;
+import avi.mod.skrim.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemSmeltedEvent;
 
-@Mod(modid = Skrim.modId, name = Skrim.name, version = Skrim.version, acceptedMinecraftVersions = "[1.11.0, 1.11.1, 1.11.2]")
+@Mod(modid = Skrim.modId, name = Skrim.name, version = Skrim.version, acceptedMinecraftVersions = "[1.11.1, 1.11.2]")
 public class Skrim {
 
 	@SidedProxy(serverSide = "avi.mod.skrim.proxy.ServerProxy", clientSide = "avi.mod.skrim.proxy.ClientProxy")
@@ -23,7 +18,7 @@ public class Skrim {
 
 	public static final String modId = "skrim";
 	public static final String name = "Skrim";
-	public static final String version = "1.1.6";
+	public static final String version = "1.1.8";
 
 	public static final TestTab creativeTab = new TestTab();
 
