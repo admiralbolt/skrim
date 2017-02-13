ac81102ca7ea8c218a13898ac1c4ddf2bfa13132
aknecht2 <aknecht2@unl.edu>
2017-02-12 18:36:36 -0600

    Actually check to see if someone is wearing the fox mask before turning them invisible.

--- src/main/java/avi/mod/skrim/items/artifacts/FoxMask.java
+++ src/main/java/avi/mod/skrim/items/artifacts/FoxMask.java
@@ -19,23 +19,23 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 
 public class FoxMask extends ArtifactArmor {
-	
+
 	public FoxMask() {
 		super("fox_mask", EntityEquipmentSlot.HEAD);
 	}
-	
+
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean par4) {
 		tooltip.add("§4Increase jump height, speed, and grants night vision.§r");
 		tooltip.add("§4Sneaking grants invisibility.§r");
 		tooltip.add("§e\"I'll wear my guy fox mask.\"");
 	}
-	
+
 	/**
 	 * Handlers for boots of springheel jack
 	 */
 	public static class FoxHandler {
-		
+
 		public static List<Potion> effects = new ArrayList<Potion>();
 		public static Map<Potion, Integer> effectStrength = new HashMap<Potion, Integer>();
 		static {
@@ -48,13 +48,13 @@ public class FoxMask extends ArtifactArmor {
 			effects.add(MobEffects.SPEED);
 			effectStrength.put(MobEffects.SPEED, 1);
 		}
-		
+
 		public static void beAFox(LivingUpdateEvent event) {
 			Entity entity = event.getEntity();
 			if (entity instanceof EntityPlayer) {
 				EntityPlayer player = (EntityPlayer) entity;
-				if (player.world.getTotalWorldTime() % 60L == 0L && !player.world.isRemote) {
-					if (Utils.isWearingArmor(player, ModItems.FOX_MASK)) {
+				if (Utils.isWearingArmor(player, ModItems.FOX_MASK)) {
+					if (player.world.getTotalWorldTime() % 60L == 0L && !player.world.isRemote) {
 						for (Potion potion : effects) {
 							PotionEffect newEffect = new PotionEffect(potion, 80, effectStrength.get(potion), true, false);
 							Utils.addOrCombineEffect(player, newEffect);
@@ -64,15 +64,11 @@ public class FoxMask extends ArtifactArmor {
 				if (!player.world.isRemote && player.isSneaking()) {
 					PotionEffect activeEffect = player.getActivePotionEffect(MobEffects.INVISIBILITY);
 					PotionEffect newEffect = new PotionEffect(MobEffects.INVISIBILITY, 3, 0, true, false);
-					if (activeEffect != null) {
-						activeEffect.combine(newEffect);
-					} else {
-						player.addPotionEffect(newEffect);
-					}
+					Utils.addOrCombineEffect(player, newEffect);
 				}
 			}
 		}
-		
+
 	}
 
 }
