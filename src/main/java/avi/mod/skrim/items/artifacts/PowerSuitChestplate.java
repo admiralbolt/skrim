8ceafb11a8f7bdc80e23e92d14a87257367f9956
admiralbolt <aviknecht@gmail.com>
2019-04-09 18:52:08 -0700

    Fixed powersuit chestplate.

--- src/main/java/avi/mod/skrim/items/artifacts/PowerSuitChestplate.java
+++ src/main/java/avi/mod/skrim/items/artifacts/PowerSuitChestplate.java
@@ -1,9 +1,5 @@
 package avi.mod.skrim.items.artifacts;
 
-import java.util.List;
-
-import javax.annotation.Nullable;
-
 import avi.mod.skrim.client.audio.ShineSparkSound;
 import avi.mod.skrim.items.ModItems;
 import avi.mod.skrim.items.armor.ArtifactArmor;
@@ -13,66 +9,99 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ * One of the metroid artifacts. Power suit chestplate allows players to shine spark.
+ * <p>
+ * Activated by sprinting for at least 5 seconds granting a massive speed boost until the player slows down.
+ */
 public class PowerSuitChestplate extends ArtifactArmor {
-	
-	public int sprintingTicks = 0;
-	public boolean spark = false;
 
-	public PowerSuitChestplate() {
-		super("powersuit_chestplate", EntityEquipmentSlot.CHEST);
-	}
+  // This is pretty fucking fast.
+  private static double MAX_SPEED = 3;
+  private static float YAW_BOUND = 10.0F;
 
-	@Override
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		tooltip.add("§4Sprinting for an extended duration activates your speed booster.§r");
-		tooltip.add("§e\"Time and reality swirl together like estuary waters.\"");
-	}
-	
-	@SideOnly(Side.CLIENT)
-	public void playShineSpark(EntityPlayer player) {
-		Minecraft.getMinecraft().getSoundHandler().playSound(new ShineSparkSound(player));
-	}
-	
+  private int sprintingTicks = 0;
+  public boolean spark = false;
 
-	public static void applyChozoTech(LivingUpdateEvent event) {
-		Entity entity = event.getEntity();
-		if (entity instanceof EntityPlayer) {
-			EntityPlayer player = (EntityPlayer) entity;
-			if (player.world.isRemote && Utils.isWearingArmor(player, ModItems.POWER_SUIT_CHESTPLATE)) {
-				ItemStack armorStack = Utils.getArmor(player, EntityEquipmentSlot.CHEST);
-				Item armor = armorStack.getItem();
-				if (armor instanceof PowerSuitChestplate) {
-					PowerSuitChestplate chozoChest = (PowerSuitChestplate) armor;
-					// Shine spark
-					if (player.isSprinting() && chozoChest.sprintingTicks >= 100) {
-						if (!chozoChest.spark) {
-							chozoChest.spark = true;
-							chozoChest.playShineSpark(player);
-						}
-						if (player.onGround) {
-							player.motionX *= 1.75;
-							player.motionZ *= 1.75;
-						} else {
-							player.motionX *= 1.75;
-							player.motionZ *= 1.75;
-							chozoChest.sprintingTicks = 0;
-						}
-					} else if (player.isSprinting() && player.onGround) {
-						chozoChest.sprintingTicks++;
-					} else {
-						chozoChest.sprintingTicks = 0;
-						chozoChest.spark = false;
-					}
-				}
-			}
-		}
-	}
+  // Saves the current speed for when jumps activate.
+  private double savedMotionX;
+  private double savedMotionZ;
+
+  // Saves the initial direction for sparking purposes.
+  private Float initialDirection = null;
+
+  public PowerSuitChestplate() {
+    super("powersuit_chestplate", EntityEquipmentSlot.CHEST);
+  }
+
+  @Override
+  public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+    tooltip.add("§4Sprinting for an extended duration activates your speed booster.§r");
+    tooltip.add("§e\"Time and reality swirl together like estuary waters.\"");
+  }
+
+  @SideOnly(Side.CLIENT)
+  private void playShineSpark(EntityPlayer player) {
+    Minecraft.getMinecraft().getSoundHandler().playSound(new ShineSparkSound(player));
+  }
+
+  public static void applyChozoTech(LivingUpdateEvent event) {
+    Entity entity = event.getEntity();
+    if (!(entity instanceof EntityPlayer)) return;
+
+    EntityPlayer player = (EntityPlayer) entity;
+    if (!player.world.isRemote || !Utils.isWearingArmor(player, ModItems.POWER_SUIT_CHESTPLATE)) return;
+
+    PowerSuitChestplate chozoChest = (PowerSuitChestplate) Utils.getArmor(player, EntityEquipmentSlot.CHEST).getItem();
+    if (chozoChest.spark) {
+      // Store the initial motion direction.
+      if (chozoChest.initialDirection == null) {
+        chozoChest.initialDirection = player.rotationYaw % 360;
+      }
+      player.rotationYaw = chozoChest.initialDirection;
+
+      if (player.onGround) {
+        // Run faster.
+        player.motionX *= 1.75;
+        player.motionZ *= 1.75;
+
+        // Scale down speed to max speed if over it.
+        double speed = Math.sqrt(player.motionX * player.motionX + player.motionZ * player.motionZ);
+        if (speed > MAX_SPEED) {
+          double scale = speed / MAX_SPEED;
+          player.motionX /= scale;
+          player.motionY /= scale;
+        }
+        chozoChest.savedMotionX = player.motionX;
+        chozoChest.savedMotionZ = player.motionZ;
+      } else {
+        // Maintain air speed.
+        player.motionX = chozoChest.savedMotionX;
+        player.motionZ = chozoChest.savedMotionZ;
+      }
+
+      // If the player slows down OR changes direction significantly.
+      if (Math.sqrt(player.motionX * player.motionX + player.motionZ * player.motionZ) < 0.25) {
+        chozoChest.sprintingTicks = 0;
+        chozoChest.spark = false;
+        chozoChest.initialDirection = null;
+      }
+    } else if (player.isSprinting() && player.onGround) {
+      chozoChest.sprintingTicks++;
+      if (chozoChest.sprintingTicks >= 100) {
+        chozoChest.spark = true;
+        chozoChest.playShineSpark(player);
+      }
+    }
+  }
 
 }
--- src/main/java/avi/mod/skrim/utils/Utils.java
+++ src/main/java/avi/mod/skrim/utils/Utils.java
@@ -1,30 +1,10 @@
 package avi.mod.skrim.utils;
 
-import java.text.DecimalFormat;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
-
-import avi.mod.skrim.blocks.SkrimBlocks;
-import com.google.common.collect.Maps;
-
 import avi.mod.skrim.Skrim;
+import avi.mod.skrim.blocks.SkrimBlocks;
 import avi.mod.skrim.skills.Skill;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockDirt;
-import net.minecraft.block.BlockDoublePlant;
-import net.minecraft.block.BlockGrass;
-import net.minecraft.block.BlockGravel;
-import net.minecraft.block.BlockMelon;
-import net.minecraft.block.BlockMycelium;
-import net.minecraft.block.BlockNewLog;
-import net.minecraft.block.BlockOldLog;
-import net.minecraft.block.BlockPumpkin;
-import net.minecraft.block.BlockRedFlower;
-import net.minecraft.block.BlockSand;
-import net.minecraft.block.BlockSoulSand;
-import net.minecraft.block.BlockYellowFlower;
+import com.google.common.collect.Maps;
+import net.minecraft.block.*;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
@@ -49,165 +29,170 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
+import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
 public class Utils {
 
-	public static String[] tuplets = { "zero-adsf", "one-asdf", "double", "triple", "quadruple", "quintuple", "sextuple", "septuple", "octople", "nontople",
-			"decuple" };
-	public static Random rand = new Random();
-	public static DecimalFormat oneDigit = new DecimalFormat("0.0");
-	public static DecimalFormat twoDigit = new DecimalFormat("0.00");
-	public static HashSet<Potion> negativeEffects = new HashSet<Potion>();
-	static {
-		negativeEffects.add(MobEffects.BLINDNESS);
-		negativeEffects.add(MobEffects.GLOWING);
-		negativeEffects.add(MobEffects.HUNGER);
-		negativeEffects.add(MobEffects.LEVITATION);
-		negativeEffects.add(MobEffects.MINING_FATIGUE);
-		negativeEffects.add(MobEffects.NAUSEA);
-		negativeEffects.add(MobEffects.POISON);
-		negativeEffects.add(MobEffects.SLOWNESS);
-		negativeEffects.add(MobEffects.UNLUCK);
-		negativeEffects.add(MobEffects.WEAKNESS);
-		negativeEffects.add(MobEffects.WITHER);
-	}
+  public static String[] tuplets = {"zero-adsf", "one-asdf", "double", "triple", "quadruple", "quintuple", "sextuple", "septuple",
+			"octople", "nontople",
+      "decuple"};
+  public static Random rand = new Random();
+  public static DecimalFormat oneDigit = new DecimalFormat("0.0");
+  public static DecimalFormat twoDigit = new DecimalFormat("0.00");
+  public static HashSet<Potion> negativeEffects = new HashSet<Potion>();
 
-	public static boolean isPointInRegion(int left, int top, int right, int bottom, int pointX, int pointY) {
-		return (pointX > left && pointX < right && pointY > top && pointY < bottom);
-	}
+  static {
+    negativeEffects.add(MobEffects.BLINDNESS);
+    negativeEffects.add(MobEffects.GLOWING);
+    negativeEffects.add(MobEffects.HUNGER);
+    negativeEffects.add(MobEffects.LEVITATION);
+    negativeEffects.add(MobEffects.MINING_FATIGUE);
+    negativeEffects.add(MobEffects.NAUSEA);
+    negativeEffects.add(MobEffects.POISON);
+    negativeEffects.add(MobEffects.SLOWNESS);
+    negativeEffects.add(MobEffects.UNLUCK);
+    negativeEffects.add(MobEffects.WEAKNESS);
+    negativeEffects.add(MobEffects.WITHER);
+  }
 
-	public static String snakeCase(String str) {
-		return str.toLowerCase().replace(" ", "_");
-	}
+  public static boolean isPointInRegion(int left, int top, int right, int bottom, int pointX, int pointY) {
+    return (pointX > left && pointX < right && pointY > top && pointY < bottom);
+  }
 
-	public static String getBlockName(Block block) {
-		return snakeCase(block.getLocalizedName());
-	}
+  public static String snakeCase(String str) {
+    return str.toLowerCase().replace(" ", "_");
+  }
 
-	public static void logBlockState(IBlockState state) {
-		Block block = state.getBlock();
-		System.out.println("harvestTool: " + block.getHarvestTool(state) + ", NAME: " + getBlockName(block) + ", class: " + block.getClass());
-	}
+  public static String getBlockName(Block block) {
+    return snakeCase(block.getLocalizedName());
+  }
 
-	public static void logHurtEvent(LivingHurtEvent event) {
-		DamageSource source = event.getSource();
-		System.out.println("source.damageType: " + source.damageType + ", damageAmount: " + event.getAmount() + ", isExplosion: " + source.isExplosion()
-				+ ", isFire: " + source.isFireDamage() + ", isMagic: " + source.isMagicDamage() + ", isProjectile: " + source.isProjectile());
-	}
+  public static void logBlockState(IBlockState state) {
+    Block block = state.getBlock();
+    System.out.println("harvestTool: " + block.getHarvestTool(state) + ", NAME: " + getBlockName(block) + ", class: " + block.getClass());
+  }
 
-	public static String getFortuneString(int fortuneAmount) {
-		return tuplets[fortuneAmount];
-	}
+  public static void logHurtEvent(LivingHurtEvent event) {
+    DamageSource source = event.getSource();
+    System.out.println("source.damageType: " + source.damageType + ", damageAmount: " + event.getAmount() + ", isExplosion: " + source.isExplosion()
+        + ", isFire: " + source.isFireDamage() + ", isMagic: " + source.isMagicDamage() + ", isProjectile: " + source.isProjectile());
+  }
 
-	public static int gaussianSum(int n) {
-		return (n * n + n) / 2;
-	}
+  public static String getFortuneString(int fortuneAmount) {
+    return tuplets[fortuneAmount];
+  }
 
-	public static String formatPercent(double percent) {
-		return (String) oneDigit.format(percent * 100);
-	}
+  public static int gaussianSum(int n) {
+    return (n * n + n) / 2;
+  }
 
-	public static String formatPercentTwo(double percent) {
-		return (String) twoDigit.format(percent * 100);
-	}
+  public static String formatPercent(double percent) {
+    return (String) oneDigit.format(percent * 100);
+  }
 
-	// registerPotionAttributeModifier(SharedMonsterAttributes.MAX_HEALTH, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", 4.0D, 0).setBeneficial());
+  public static String formatPercentTwo(double percent) {
+    return (String) twoDigit.format(percent * 100);
+  }
 
-	private final Map<IAttribute, AttributeModifier> attributeModifierMap = Maps.<IAttribute, AttributeModifier>newHashMap();
+  // registerPotionAttributeModifier(SharedMonsterAttributes.MAX_HEALTH, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", 4.0D, 0).setBeneficial());
 
-	public static void applyAttributesModifiersToEntity(EntityLivingBase entityLivingBaseIn, Map<IAttribute, AttributeModifier> attributeMap, int amplifier) {
-		AbstractAttributeMap entityAttributes = entityLivingBaseIn.getAttributeMap();
-		for (Entry<IAttribute, AttributeModifier> entry : attributeMap.entrySet()) {
-			IAttributeInstance iattributeinstance = entityAttributes.getAttributeInstance((IAttribute) entry.getKey());
-			if (iattributeinstance != null) {
-				AttributeModifier attributemodifier = (AttributeModifier) entry.getValue();
-				iattributeinstance.removeModifier(attributemodifier);
-				iattributeinstance.applyModifier(new AttributeModifier(attributemodifier.getID(), attributemodifier.getName(),
-						getAttributeModifierAmount(amplifier, attributemodifier), attributemodifier.getOperation()));
-			}
-		}
-	}
+  private final Map<IAttribute, AttributeModifier> attributeModifierMap = Maps.<IAttribute, AttributeModifier>newHashMap();
 
-	public static double getAttributeModifierAmount(int amplifier, AttributeModifier modifier) {
-		return modifier.getAmount() * (double) (amplifier + 1);
-	}
+  public static void applyAttributesModifiersToEntity(EntityLivingBase entityLivingBaseIn,
+																											Map<IAttribute, AttributeModifier> attributeMap, int amplifier) {
+    AbstractAttributeMap entityAttributes = entityLivingBaseIn.getAttributeMap();
+    for (Entry<IAttribute, AttributeModifier> entry : attributeMap.entrySet()) {
+      IAttributeInstance iattributeinstance = entityAttributes.getAttributeInstance((IAttribute) entry.getKey());
+      if (iattributeinstance != null) {
+        AttributeModifier attributemodifier = (AttributeModifier) entry.getValue();
+        iattributeinstance.removeModifier(attributemodifier);
+        iattributeinstance.applyModifier(new AttributeModifier(attributemodifier.getID(), attributemodifier.getName(),
+            getAttributeModifierAmount(amplifier, attributemodifier), attributemodifier.getOperation()));
+      }
+    }
+  }
 
-	public static boolean isRawXpBlock(Block block) {
-		return (block instanceof BlockOldLog || block instanceof BlockNewLog || block instanceof BlockRedFlower || block instanceof BlockYellowFlower
-				|| block instanceof BlockSand || block instanceof BlockGravel || block instanceof BlockDirt || block instanceof BlockMycelium
-				|| block instanceof BlockGrass || block instanceof BlockSoulSand || block instanceof BlockPumpkin || block instanceof BlockMelon
-				|| block instanceof BlockDoublePlant || block == Blocks.IRON_ORE || block == Blocks.GOLD_ORE || block == Blocks.DIAMOND_ORE
-				|| block == Blocks.LAPIS_ORE || block == Blocks.REDSTONE_ORE || block == Blocks.COAL_ORE || block == Blocks.EMERALD_ORE
-				|| block == SkrimBlocks.WEIRWOOD_WOOD);
-	}
+  public static double getAttributeModifierAmount(int amplifier, AttributeModifier modifier) {
+    return modifier.getAmount() * (double) (amplifier + 1);
+  }
 
-	public static boolean isSilkTouching(BlockEvent.BreakEvent event) {
-		EntityPlayer player = event.getPlayer();
-		ItemStack mainStack = player.getHeldItemMainhand();
-		int silkTouch = EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, mainStack);
-		return silkTouch > 0;
-	}
+  public static boolean isRawXpBlock(Block block) {
+    return (block instanceof BlockOldLog || block instanceof BlockNewLog || block instanceof BlockRedFlower || block instanceof BlockYellowFlower
+        || block instanceof BlockSand || block instanceof BlockGravel || block instanceof BlockDirt || block instanceof BlockMycelium
+        || block instanceof BlockGrass || block instanceof BlockSoulSand || block instanceof BlockPumpkin || block instanceof BlockMelon
+        || block instanceof BlockDoublePlant || block == Blocks.IRON_ORE || block == Blocks.GOLD_ORE || block == Blocks.DIAMOND_ORE
+        || block == Blocks.LAPIS_ORE || block == Blocks.REDSTONE_ORE || block == Blocks.COAL_ORE || block == Blocks.EMERALD_ORE
+        || block == SkrimBlocks.WEIRWOOD_WOOD);
+  }
 
-	public static boolean isNegativeEffect(PotionEffect effect) {
-		Potion potion = (Potion) ReflectionUtils.getPrivateField(effect, "potion", "field_188420_b");
-		return (potion != null && negativeEffects.contains(potion));
-	}
+  public static boolean isSilkTouching(BlockEvent.BreakEvent event) {
+    EntityPlayer player = event.getPlayer();
+    ItemStack mainStack = player.getHeldItemMainhand();
+    int silkTouch = EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, mainStack);
+    return silkTouch > 0;
+  }
 
-	public static boolean isWearingArmor(EntityPlayer player, ItemArmor armor) {
-		InventoryPlayer inventory = player.inventory;
-		if (inventory != null) {
-			ItemStack stack = inventory.armorInventory.get(armor.armorType.getIndex());
-			if (stack != null) {
-				Item targetItem = stack.getItem();
-				if (targetItem == armor) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+  public static boolean isNegativeEffect(PotionEffect effect) {
+    Potion potion = (Potion) ReflectionUtils.getPrivateField(effect, "potion", "field_188420_b");
+    return (potion != null && negativeEffects.contains(potion));
+  }
 
-	public static ItemStack getArmor(EntityPlayer player, EntityEquipmentSlot armorType) {
-		return player.inventory.armorInventory.get(armorType.getIndex());
-	}
+  public static boolean isWearingArmor(EntityPlayer player, ItemArmor armor) {
+    InventoryPlayer inventory = player.inventory;
+    if (inventory != null) {
+      ItemStack stack = inventory.armorInventory.get(armor.armorType.getIndex());
+      Item targetItem = stack.getItem();
+      return targetItem == armor;
+    }
+    return false;
+  }
 
-	public static void log(String message) {
-		if (Skrim.DEBUG) {
-			System.out.println(message);
-		}
-	}
+  public static ItemStack getArmor(EntityPlayer player, EntityEquipmentSlot armorType) {
+    return player.inventory.armorInventory.get(armorType.getIndex());
+  }
 
-	public static void logSkillEvent(Event event, Skill skill, String message) {
-		log("[" + getEventName(event) + "](" + skill.name + ") " + message);
-	}
+  public static void log(String message) {
+    if (Skrim.DEBUG) {
+      System.out.println(message);
+    }
+  }
 
-	public static String getEventName(Event event) {
-		String className = event.getClass().getName();
-		String[] split = className.split("\\.");
-		return split[split.length - 1];
-	}
+  public static void logSkillEvent(Event event, Skill skill, String message) {
+    log("[" + getEventName(event) + "](" + skill.name + ") " + message);
+  }
 
-	public static void addOrCombineEffect(EntityPlayer player, PotionEffect effect) {
-		PotionEffect activeEffect = player.getActivePotionEffect(effect.getPotion());
-		if (activeEffect != null) {
-			activeEffect.combine(effect);
-		} else {
-			activeEffect = effect;
-		}
-		player.addPotionEffect(activeEffect);
-	}
+  public static String getEventName(Event event) {
+    String className = event.getClass().getName();
+    String[] split = className.split("\\.");
+    return split[split.length - 1];
+  }
 
-	public static int getNumberOfItems(NonNullList<ItemStack> inventory) {
-		int size = 0;
-		for (ItemStack stack : inventory) {
-			if (stack != ItemStack.EMPTY) {
-				size++;
-			}
-		}
-		return size;
-	}
+  public static void addOrCombineEffect(EntityPlayer player, PotionEffect effect) {
+    PotionEffect activeEffect = player.getActivePotionEffect(effect.getPotion());
+    if (activeEffect != null) {
+      activeEffect.combine(effect);
+    } else {
+      activeEffect = effect;
+    }
+    player.addPotionEffect(activeEffect);
+  }
 
-	public static boolean areSimilarStacks(ItemStack stack1, ItemStack stack2) {
-		return (stack1.getItem() == stack2.getItem() && stack1.getMetadata() == stack2.getMetadata() && stack1 != ItemStack.EMPTY && stack2 != ItemStack.EMPTY && stack1.getCount() > 0 && stack2.getCount() > 0);
-	}
+  public static int getNumberOfItems(NonNullList<ItemStack> inventory) {
+    int size = 0;
+    for (ItemStack stack : inventory) {
+      if (stack != ItemStack.EMPTY) {
+        size++;
+      }
+    }
+    return size;
+  }
+
+  public static boolean areSimilarStacks(ItemStack stack1, ItemStack stack2) {
+    return (stack1.getItem() == stack2.getItem() && stack1.getMetadata() == stack2.getMetadata() && stack1 != ItemStack.EMPTY && stack2 != ItemStack.EMPTY && stack1.getCount() > 0 && stack2.getCount() > 0);
+  }
 
 }
