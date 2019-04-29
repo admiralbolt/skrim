fc5626f3785c3565488e79a598969b19313fee0f
admiralbolt <aviknecht@gmail.com>
2019-04-29 15:23:59 -0700

    Some minor cleanup of add treasure. Added artifacts to the ender dragon loot pool.

--- src/main/java/avi/mod/skrim/world/loot/AddTreasure.java
+++ src/main/java/avi/mod/skrim/world/loot/AddTreasure.java
@@ -1,35 +1,37 @@
 package avi.mod.skrim.world.loot;
 
+import com.google.common.collect.ImmutableList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.event.LootTableLoadEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AddTreasure {
 
-  public static List<ResourceLocation> chests = new ArrayList<ResourceLocation>();
+  private static List<ResourceLocation> LOOT_TABLES = ImmutableList.of(
+      LootTableList.CHESTS_ABANDONED_MINESHAFT,
+      LootTableList.CHESTS_END_CITY_TREASURE,
+      LootTableList.CHESTS_IGLOO_CHEST,
+      LootTableList.CHESTS_DESERT_PYRAMID,
+      LootTableList.CHESTS_JUNGLE_TEMPLE,
+      LootTableList.CHESTS_JUNGLE_TEMPLE_DISPENSER,
+      LootTableList.CHESTS_NETHER_BRIDGE,
+      LootTableList.CHESTS_SIMPLE_DUNGEON,
+      LootTableList.CHESTS_SPAWN_BONUS_CHEST,
+      LootTableList.CHESTS_STRONGHOLD_CORRIDOR,
+      LootTableList.CHESTS_STRONGHOLD_CROSSING,
+      LootTableList.CHESTS_STRONGHOLD_LIBRARY,
+      LootTableList.CHESTS_VILLAGE_BLACKSMITH,
+      LootTableList.CHESTS_WOODLAND_MANSION,
+      LootTableList.ENTITIES_ENDER_DRAGON
+  );
 
-  static {
-    chests.add(LootTableList.CHESTS_ABANDONED_MINESHAFT);
-    chests.add(LootTableList.CHESTS_END_CITY_TREASURE);
-    chests.add(LootTableList.CHESTS_IGLOO_CHEST);
-    chests.add(LootTableList.CHESTS_DESERT_PYRAMID);
-    chests.add(LootTableList.CHESTS_JUNGLE_TEMPLE);
-    chests.add(LootTableList.CHESTS_JUNGLE_TEMPLE_DISPENSER);
-    chests.add(LootTableList.CHESTS_NETHER_BRIDGE);
-    chests.add(LootTableList.CHESTS_SIMPLE_DUNGEON);
-    chests.add(LootTableList.CHESTS_SPAWN_BONUS_CHEST);
-    chests.add(LootTableList.CHESTS_STRONGHOLD_CORRIDOR);
-    chests.add(LootTableList.CHESTS_STRONGHOLD_CROSSING);
-    chests.add(LootTableList.CHESTS_STRONGHOLD_LIBRARY);
-    chests.add(LootTableList.CHESTS_VILLAGE_BLACKSMITH);
-    chests.add(LootTableList.CHESTS_WOODLAND_MANSION);
-  }
+
 
   public static void addTreasure(LootTableLoadEvent event) {
-    if (!chests.contains(event.getName())) return;
+    if (!LOOT_TABLES.contains(event.getName())) return;
+
     event.getTable().addPool(DynamicLootPool.ARTIFACT_POOL);
     event.getTable().addPool(DynamicLootPool.RECORD_POOL);
   }
