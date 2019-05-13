b217599aa175cd6a85b60e7666c9e52050a03684
admiralbolt <aviknecht@gmail.com>
2019-05-12 17:46:29 -0700

    Fixed mega chests for multiplayer.

--- src/main/java/avi/mod/skrim/proxy/ClientProxy.java
+++ src/main/java/avi/mod/skrim/proxy/ClientProxy.java
@@ -1,9 +1,7 @@
 package avi.mod.skrim.proxy;
 
-import avi.mod.skrim.Skrim;
 import avi.mod.skrim.client.renderer.CustomRenderers;
 import avi.mod.skrim.handlers.GuiEventHandler;
-import avi.mod.skrim.handlers.GuiHandler;
 import avi.mod.skrim.handlers.SkrimEntitySpawnHandler;
 import avi.mod.skrim.items.SkrimItems;
 import net.minecraft.client.Minecraft;
@@ -12,7 +10,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 import javax.annotation.Nullable;
@@ -39,7 +36,6 @@ public class ClientProxy implements IProxy {
   public void init() {
     SkrimEntitySpawnHandler.init();
     MinecraftForge.EVENT_BUS.register(new GuiEventHandler());
-    NetworkRegistry.INSTANCE.registerGuiHandler(Skrim.instance, new GuiHandler());
   }
 
   @Override
