dbbb45e08a08877be113c45d51967ca2b89b6421
admiralbolt <aviknecht@gmail.com>
2019-05-20 19:08:53 -0700

    Replaced the base game brewing stand with the new skrim verison.

--- src/main/java/avi/mod/skrim/items/items/SkrimBrewingStandItem.java
+++ src/main/java/avi/mod/skrim/items/items/SkrimBrewingStandItem.java
@@ -1,8 +1,16 @@
 package avi.mod.skrim.items.items;
 
+import avi.mod.skrim.Skrim;
 import avi.mod.skrim.blocks.SkrimBlocks;
 import avi.mod.skrim.items.ItemBase;
+import avi.mod.skrim.items.SkrimItems;
+import avi.mod.skrim.utils.Utils;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemBlockSpecial;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class SkrimBrewingStandItem extends ItemBlockSpecial implements ItemBase {
 
@@ -17,5 +25,21 @@ public class SkrimBrewingStandItem extends ItemBlockSpecial implements ItemBase 
   @Override
   public String getTexturePath() {
     return "items";
+  }
+
+  @Mod.EventBusSubscriber(modid = Skrim.MOD_ID)
+  public static class Handler {
+
+    @SubscribeEvent
+    public static void pickupBrewingStand(PlayerEvent.ItemPickupEvent event) {
+      ItemStack stack = event.getStack();
+      if (stack.getItem() == Items.BREWING_STAND) {
+        Utils.removeFromInventory(event.player.inventory, Items.BREWING_STAND, stack.getCount());
+        ItemStack newStack = new ItemStack(SkrimItems.BREWING_STAND, stack.getCount(), stack.getMetadata());
+        newStack.setTagCompound(stack.getTagCompound());
+        event.player.addItemStackToInventory(newStack);
+      }
+    }
+
   }
 }
